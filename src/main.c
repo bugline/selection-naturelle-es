277f868319505e6e1e093b28e5dabdcebcea944c
DESSARD Malo Jean-Lionel <DESSARDM@0440029t.paysdelaloire.education>
2022-04-29 10:48:17 +0200

    ajout de game-maker

--- src/main.c
+++ src/main.c
@@ -1,7 +1,40 @@
 #include <stdio.h>
 
+#include "core.h"
+
+void MainInit(App *p_App)
+{
+
+}
+
+void MainUpdate(App *p_App, float p_Dt)
+{
+
+}
+
+void MainRender(App *p_App)
+{
+
+}
+
+void MainRemove(App *p_App)
+{
+
+}
+
 int main()
 {
+	App_param param = App_param_default();
+
+	App *app = App_new(param);
+
+	App_setInit(app, &MainInit);
+	App_setUpdate(app, &MainUpdate);
+	App_setRender(app, &MainRender);
+	App_setRemove(app, &MainRemove);
+
+	App_loop(app);
+	App_del(app);
 	printf("Hello World!");
 	return 0;
 }
