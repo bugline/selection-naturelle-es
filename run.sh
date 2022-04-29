559eeffea14b1ead72e2e9ebb1d327b871136dc5
yuesubi <yuesubi.zarbi@gmail.com>
2022-04-29 17:52:56 +0200

    added build tools for linux

---
+++ run.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# VARIABLES
+OUT=bin/main.exe
+INP='src/main.c'
+
+INCLUDE=inc/
+LIB=lib/
+
+
+# CLEAN THE PROMPT
+clear
+
+
+# DELETE PREVIOUS BUILD
+if [ -f "$OUT" ]; then
+    echo "[RUN] Deleting the previous $OUT ..."
+    rm "$OUT"
+    echo ""
+fi
+
+
+# COMPILE
+echo "[RUN] Compiling $INP ..."
+gcc $INP -o $OUT -Wall -I $INCLUDE -L $LIB -lraylib -lGL -lm -lpthread -ldl -lrt -lX11
+echo ""
+
+
+# RUN
+if [ -f "$OUT" ]; then
+    echo "[RUN] Running $OUT ..."
+    exec $OUT
+    echo ""
+else
+    echo "[RUN] Compilation of $INP failed !"
+    echo ""
+fi
